211fa676c719e530d30ed20b61c6f74dd36f559f
Compu Care <Compu Care@DESKTOP-DN4BUBJ>
2025-11-06 19:17:49 +0200

    Update

--- TicketWave/Areas/Identity/Controllers/AccountController.cs
+++ TicketWave/Areas/Identity/Controllers/AccountController.cs
@@ -1,18 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using TicketWave.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
-using StackExchange.Profiling.Data;
-using System.Security.Cryptography;
 using System.Threading.Tasks;
 using TicketWave.Models;
 using TicketWave.Repositories.IRepositories;
 using TicketWave.Utitlies;
 using TicketWave.ViewModel;
 using TicketWave.ViewModels;
-using Umbraco.Core.Models.Membership;
-using static System.Net.WebRequestMethods;
 
 namespace TicketWave.Areas.Identity.Controllers
 {
@@ -20,16 +17,16 @@ namespace TicketWave.Areas.Identity.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly IEmailSender emailSender;
-        private readonly IRepository<ApplicationUserOTP> applicationUserOTPRepository;
         private readonly SignInManager<ApplicationUser> _signInManager;
-        private string userId;
+        private readonly IEmailSender _emailSender;
+        private readonly IRepository<ApplicationUserOTP> _applicationUserOTPRepository;
 
-        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager , IEmailSender emailSender , IRepository<ApplicationUserOTP> applicationUserOTPRepository)
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IEmailSender emailSender, IRepository<ApplicationUserOTP> applicationUserOTPRepository)
         {
             _userManager = userManager;
-            this.emailSender = emailSender;
-            this.applicationUserOTPRepository = applicationUserOTPRepository;
+            _signInManager = signInManager;
+            _emailSender = emailSender;
+            _applicationUserOTPRepository = applicationUserOTPRepository;
         }
 
         public async Task<IActionResult> Logout()
@@ -37,6 +34,7 @@ namespace TicketWave.Areas.Identity.Controllers
             await _signInManager.SignOutAsync();
             return RedirectToAction("Login");
         }
+
         public IActionResult Register()
         {
             return View();
@@ -47,6 +45,7 @@ namespace TicketWave.Areas.Identity.Controllers
         {
             if (!ModelState.IsValid)
                 return View(registerVM);
+
             var user = new ApplicationUser()
             {
                 FirstName = registerVM.FirstName,
@@ -66,19 +65,24 @@ namespace TicketWave.Areas.Identity.Controllers
 
                 return View(registerVM);
             }
-            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            var link = Url.Action(nameof(confirmEmail), "Account", new { area = "Identity" }, token, userId = user.Id);
-            await emailSender.SendEmailAsync(registerVM.Email, "TicketWave- Confirm Your Email!"
-               , $"<h1>Confirm Your Email By Clicking <a href='{link}'>Here</a></h1>");
-            await _userManager.AddToRoleAsync(user, SD.CUSTOMER_ROLE);
-            return RedirectToAction("Login");
 
+            // Send Confirmation Mail
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            var link = Url.Action(nameof(ConfirmEmail), "Account", new { area = "Identity", token, userId = user.Id }, Request.Scheme);
+
+            await _emailSender.SendEmailAsync(registerVM.Email, "Ecommerce 519 - Confirm Your Email!"
+                , $"<h1>Confirm Your Email By Clicking <a href='{link}'>Here</a></h1>");
+
+            await _userManager.AddToRoleAsync(user, SD.CUSTOMER_ROLE);
+
+            return RedirectToAction("Login");
         }
 
         public IActionResult Login()
         {
             return View();
         }
+
         [HttpPost]
         public async Task<IActionResult> Login(LoginVM loginVM)
         {
@@ -109,10 +113,13 @@ namespace TicketWave.Areas.Identity.Controllers
 
             return RedirectToAction("Index", "Home", new { area = "Admin" });
         }
+
         public IActionResult ResendEmailConfirmation()
         {
             return View();
         }
+
+        [HttpPost]
         public async Task<IActionResult> ResendEmailConfirmation(ResendEmailConfirmationVM resendEmailConfirmationVM)
         {
             if (!ModelState.IsValid)
@@ -126,7 +133,6 @@ namespace TicketWave.Areas.Identity.Controllers
                 return View(resendEmailConfirmationVM);
             }
 
-
             if (user.EmailConfirmed)
             {
                 ModelState.AddModelError(string.Empty, "Already Confirmed!!");
@@ -137,61 +143,60 @@ namespace TicketWave.Areas.Identity.Controllers
             var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
             var link = Url.Action(nameof(ConfirmEmail), "Account", new { area = "Identity", token, userId = user.Id }, Request.Scheme);
 
-            await emailSender.SendEmailAsync(user.Email!, "TicketWave - Resend Confirm Your Email!"
+            await _emailSender.SendEmailAsync(user.Email!, "Ecommerce 519 - Resend Confirm Your Email!"
                 , $"<h1>Confirm Your Email By Clicking <a href='{link}'>Here</a></h1>");
 
             return RedirectToAction("Login");
         }
+
         public IActionResult ForgetPassword()
         {
             return View();
         }
+
         [HttpPost]
-        public async Task<IActionResult> ForgetPassword(ForgetPasswordVM ForgetPasswordVM)
+        public async Task<IActionResult> ForgetPassword(ForgetPasswordVM forgetPasswordVM)
         {
             if (!ModelState.IsValid)
-                return View(ForgetPasswordVM);
+                return View(forgetPasswordVM);
 
-            var user = await _userManager.FindByNameAsync(ForgetPasswordVM.UserNameOREmail) ?? await _userManager.FindByEmailAsync(ForgetPasswordVM.UserNameOREmail);
+            var user = await _userManager.FindByNameAsync(forgetPasswordVM.UserNameOREmail) ?? await _userManager.FindByEmailAsync(forgetPasswordVM.UserNameOREmail);
 
             if (user is null)
             {
                 ModelState.AddModelError(string.Empty, "Invalid User Name / Email");
-                return View(ForgetPasswordVM);
+                return View(forgetPasswordVM);
             }
 
+            var userOTPs = await _applicationUserOTPRepository.GetAsync(e => e.ApplicationUserId == user.Id);
 
-            if (user.EmailConfirmed)
-            {
-                ModelState.AddModelError(string.Empty, "Already Confirmed!!");
-                return View(ForgetPasswordVM);
-            }
-            var Uesrotps = await applicationUserOTPRepository.GetAsync(e => e.ApplicationUserId == user.Id);
-            var totalOTPs = Uesrotps.Count(e => (DateTime.UtcNow - e.CreateAt).TotalHours < 24);
+            var totalOTPs = userOTPs.Count(e => (DateTime.UtcNow - e.CreateAt).TotalHours < 24);
 
             if (totalOTPs > 3)
             {
                 ModelState.AddModelError(string.Empty, "Too Many Attemps");
-                return View(ForgetPasswordVM);
+                return View(forgetPasswordVM);
             }
 
-            var OTP = new Random().Next(1000, 9999).ToString();
-            await applicationUserOTPRepository.AddAsync(new()
+            var otp = new Random().Next(1000, 9999).ToString();
+
+            await _applicationUserOTPRepository.AddAsync(new()
             {
                 Id = Guid.NewGuid().ToString(),
                 ApplicationUserId = user.Id,
                 CreateAt = DateTime.UtcNow,
                 IsValid = true,
-                OTP = OTP,
+                OTP = otp,
                 ValidTo = DateTime.UtcNow.AddDays(1),
             });
-            await applicationUserOTPRepository.CommitAsync();
+            await _applicationUserOTPRepository.CommitAsync();
 
-            await emailSender.SendEmailAsync(user.Email!, "TicketWave - Reset Your Password!"
-                , $"<h1>Use This OTP: {OTP} To Reset Your Account. Don't share it.</h1>");
+            await _emailSender.SendEmailAsync(user.Email!, "Ecommerce 519 - Reset Your Password"
+                , $"<h1>Use This OTP: {otp} To Reset Your Account. Don't share it.</h1>");
 
-            return RedirectToAction("ValidateOTP");
+            return RedirectToAction("ValidateOTP", new { userId = user.Id });
         }
+
         public IActionResult ValidateOTP(string userId)
         {
             return View(new ValidateOTPVM
@@ -203,7 +208,7 @@ namespace TicketWave.Areas.Identity.Controllers
         [HttpPost]
         public async Task<IActionResult> ValidateOTP(ValidateOTPVM validateOTPVM)
         {
-            var result = await applicationUserOTPRepository.GetOneAsync(e => e.ApplicationUserId == validateOTPVM.ApplicationUserId && e.OTP == validateOTPVM.OTP && e.IsValid);
+            var result = await _applicationUserOTPRepository.GetOneAsync(e => e.ApplicationUserId == validateOTPVM.ApplicationUserId && e.OTP == validateOTPVM.OTP && e.IsValid);
 
             if (result is null)
             {
@@ -212,6 +217,7 @@ namespace TicketWave.Areas.Identity.Controllers
 
             return RedirectToAction("NewPassword", new { userId = validateOTPVM.ApplicationUserId });
         }
+
         public IActionResult NewPassword(string userId)
         {
             return View(new NewPasswordVM
@@ -249,8 +255,7 @@ namespace TicketWave.Areas.Identity.Controllers
             return RedirectToAction("Login");
         }
 
-
-        public async Task<IActionResult> confirmEmail(string userId, string token)
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
         {
             var user = await _userManager.FindByIdAsync(userId);
 
@@ -266,6 +271,6 @@ namespace TicketWave.Areas.Identity.Controllers
 
             return RedirectToAction("Login");
         }
-    }
 
+    }
 }
